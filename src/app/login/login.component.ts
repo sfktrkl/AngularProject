bc3e697bf3af6ba00628c6a378d42270657b1757
Safak TURKELI <sfktrkl@gmail.com>
2020-09-23 22:56:29 +0300

    Login form
    * Create a login form and make validation by checking user id and passwords.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+// Import form group and form control to be able to create a form
+import { FormGroup, FormControl, Validators } from '@angular/forms'
+
+// Import RestaurantService to post to API
+import { RestaurantService } from '../restaurant.service'
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +13,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private restaurants: RestaurantService) { }
 
+  // Create a map from userId(key) and password(value)
+  users = new Map();
   ngOnInit(): void {
+    this.restaurants.getUserDataFromAPI().subscribe((data) => {
+      Object.keys(data).forEach(key => { this.users.set(data[key].userId, data[key].password) });
+    });
   }
 
+  // Create a form to be able to get values
+  loginForm = new FormGroup({
+    userId: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+  });
+
+  // Use a boolean to show alert when data is submitted.
+  alert: number = 0;
+
+  // Create a method to hide alert
+  closeAlert() {
+    this.alert = 0;
+  }
+
+  // Create a method to determine which alert will be shown
+  updateAlert() {
+    if (!this.isUserExist())
+      this.alert = 1;                 // User does not exist
+    else if (!this.isUserValid())
+      this.alert = 2;                 // Password do not match
+    else
+      this.alert = 3;                 // Successfull
+  }
+
+  // Define a method to get fields from reactive form and to validate
+  validateField(fieldName) {
+    var field = this.loginForm.get(fieldName);
+    return field.invalid && field.touched;
+  }
+
+  // Create a method to check validity of the form
+  isFormValid() {
+    return this.loginForm.touched && this.loginForm.valid;
+  }
+
+  // Check whether given user id is exist or not
+  isUserExist() {
+    return this.users.has(this.loginForm.controls['userId'].value);
+  }
+
+  // Check whether given password is corrent or not
+  isUserValid() {
+    return this.users.get(this.loginForm.controls['userId'].value) == this.loginForm.controls['password'].value;
+  }
 }
